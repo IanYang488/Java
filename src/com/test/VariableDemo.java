54350c9b7597598d61a71aa404bc58e64fd2d5ff
IanYang488 <yyang238@ucsc.edu>
2021-06-27 11:49:18 +0800

    update to P15

---
+++ src/com/test/VariableDemo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        //Define a variable
+        int a = 10;
+
+        //Output a variable
+        System.out.println(a);
+
+        //Change a variable
+        a = 20;
+        System.out.println(a);
+    }
+}
