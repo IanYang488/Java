0a802d54495a69dc2790ee07179bc7fc42d65afa
Ian Yang <59089314+IanYang488@users.noreply.github.com>
2021-07-07 14:39:42 +0800

    June 30 (#3)
    * Removed extra "Java" folder
    
    * Removed extra "Java" folder
    
    * Removed extra "Java" folder
    
    * Updated to P45
    
    * Updated to P45
    
    * Updated to P45
    
    * July_1

---
+++ src/com/test/ForTest04.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class ForTest04 {
+    public static void main(String[] args) {
+        for (int i = 100; i <= 999; i++) {
+            if (((i % 10) * (i % 10) * (i % 10) + (i / 10 % 10) * (i / 10 % 10) * (i / 10 % 10) +
+                    (i / 10 / 10 % 10) * (i / 10 / 10 % 10) * (i / 10 / 10 % 10)) == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
