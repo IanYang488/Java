920167224d02854815631f17d4db96d836cff8ef
Ian Yang <59089314+IanYang488@users.noreply.github.com>
2021-08-15 16:07:36 +0800

    August 15 (#17)
    * Removed extra "Java" folder
    
    * To P68
    
    * To P122
    
    * To P125
    
    * Basic Finished, OOP updated to P102
    
    * August 7 Update
    
    * August 10 Update
    
    * August 15 Update

--- src/Map/Demo.java
+++ src/Map/Demo.java
@@ -7,7 +7,6 @@ public class Demo {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
 
-
         map.put("001", "IA");
         map.put("002", "Ian Yang");
         map.put("002", "IA");  // invalid
